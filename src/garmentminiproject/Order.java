5cc75f4617258b85d33f8ee35e721aa2a36ffb16
Imtiazahmedoyon <ahmed2305101942@diu.edu.bd>
2024-11-14 21:54:20 +0600

    Upload to github

---
+++ src/garmentminiproject/Order.java
@@ -0,0 +1,36 @@
+
+package garmentminiproject;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+public class Order {
+    public String orderId;
+    public Date orderDate;
+    public List<Garment> garments = new ArrayList<>();
+    private double totalAmount;
+
+    void addGarment(Garment garment) {
+        garments.add(garment);
+    }
+
+    double calculateTotalAmount() {
+        for (Garment g : garments) {
+            totalAmount += g.price;
+        }
+        return totalAmount;
+    }
+
+    void printOrderDetails() {
+        System.out.println("--------------------------");
+        System.out.println("Order Details");
+        System.out.println("--------------------------");
+        for (Garment g : garments) {
+            System.out.println("Name: " + g.name);
+            System.out.println("Price: " + g.price);
+            System.out.println("Description: " + g.description);
+            System.out.println("--------------------------");
+        }
+    }
+    
+}
