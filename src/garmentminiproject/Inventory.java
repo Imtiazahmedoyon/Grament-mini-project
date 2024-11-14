57f65053d936b0399f18390b50c3519f9ea9f65f
Imtiazahmedoyon <ahmed2305101942@diu.edu.bd>
2024-11-14 21:59:33 +0600

    Upload to github

---
+++ src/garmentminiproject/Inventory.java
@@ -0,0 +1,26 @@
+
+package garmentminiproject;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Inventory {
+    List<Garment> garments = new ArrayList<>();
+
+    void addGarment(Garment garment) {
+        garments.add(garment);
+    }
+
+    void removeGarment(String id) {
+        garments.removeIf(g -> g.id.equals(id));
+    }
+
+    Garment findGarment(String id) {
+        for (Garment g : garments) {
+            if (g.id.equals(id))
+                return g;
+        }
+        return null;
+    }
+    
+}
